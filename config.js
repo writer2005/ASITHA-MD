c45250b6dccac61241f93b88a5e5b85e8ac5c779
writer2005 <rygwriter2005@gmail.com>
2024-10-03 10:30:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=XAFkHDhR#8lLkg40unMwHn1EpRh3ZP_KAHhzdMXjpdCKzh2G1M3c",
+MONGODB: process.env.MONGODB || "mongodb+srv://rygwriter2005:L9csK6k4LzPnvfIc@cluster0.fgi7g.mongodb.net/",//enter mongo db url
 };
